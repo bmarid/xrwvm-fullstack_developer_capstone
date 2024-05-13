1d9ef552513d20ed560cc9167303bc965f13df04
bmarid <mary.baj@yandex.ru>
2024-05-14 01:57:34 +0200

    style fix

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -20,14 +20,20 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('djangoapp/', include('djangoapp.urls')),
-    path('about/',TemplateView.as_view(template_name="About.html")),
-    path('contact/',TemplateView.as_view(template_name="Contact.html")),
-    # Login page is a REACT page rendered from a route 
-    # that is configured in /server/frontend/src/App.js.
-    path('login/', TemplateView.as_view(template_name="index.html")),
-    path('register/', TemplateView.as_view(template_name="index.html")),
-    path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('', TemplateView.as_view(template_name="Home.html")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('djangoapp/', include('djangoapp.urls')),
+                  path('about/',
+                       TemplateView.as_view(template_name="About.html")),
+                  path('contact/',
+                       TemplateView.as_view(template_name="Contact.html")),
+                  # Login page is a REACT page rendered from a route
+                  # that is configured in /server/frontend/src/App.js.
+                  path('login/',
+                       TemplateView.as_view(template_name="index.html")),
+                  path('register/',
+                       TemplateView.as_view(template_name="index.html")),
+                  path('dealers/',
+                       TemplateView.as_view(template_name="index.html")),
+                  path('', TemplateView.as_view(template_name="Home.html")),
+              ] + static(settings.STATIC_URL,
+                         document_root=settings.STATIC_ROOT)
