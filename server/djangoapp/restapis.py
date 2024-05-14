1a11f0515dbe7b259585441d3a40d591acad6f50
bmarid <mary.baj@yandex.ru>
2024-05-14 16:12:32 -0400

    fix for Dealers page

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,5 +1,5 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
@@ -9,7 +9,7 @@ backend_url = os.getenv(
     'backend_url', default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    default="http://localhost:5050")
 
 
 def get_request(endpoint, **kwargs):
@@ -21,13 +21,14 @@ def get_request(endpoint, **kwargs):
     request_url = backend_url + endpoint + "?" + params
 
     print("GET from {} ".format(request_url))
+    print("{}, {}".format(backend_url, endpoint))
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
     except:
         # If any error occurs
-        print("Network exception occurred")
+        print("Network get req exception occurred")
 
 
 def analyze_review_sentiments(text):
@@ -38,7 +39,7 @@ def analyze_review_sentiments(text):
         return response.json()
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
-        print("Network exception occurred")
+        print("Network analys exception occurred")
 
 
 def post_review(data_dict):
@@ -48,4 +49,4 @@ def post_review(data_dict):
         print(response.json())
         return response.json()
     except:
-        print("Network exception occurred")
+        print("Network new post exception occurred")
