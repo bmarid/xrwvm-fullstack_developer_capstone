3d6fc23963a674159eebd23b98bd5764baf76156
bmarid <mary.baj@yandex.ru>
2024-05-14 02:06:43 +0200

    style fix

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,12 +1,6 @@
-# Uncomment the required imports before adding the code
-
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
+
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -18,11 +12,10 @@ from .populate import initiate
 from .models import CarMake, CarModel
 from .restapis import get_request, analyze_review_sentiments, post_review
 
+
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
-
-# Create your views here.
 
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
