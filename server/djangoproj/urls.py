4a3d2805a0aff324c2a90e9b37b38ae7864fdaad
bmarid <mary.baj@yandex.ru>
2024-05-14 17:22:56 -0400

    py lint style fix

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -36,6 +36,8 @@ urlpatterns = [
                        TemplateView.as_view(template_name="index.html")),
                   path('dealer/<int:dealer_id>',
                        TemplateView.as_view(template_name="index.html")),
+                  path('postreview/<int:dealer_id>',
+                       TemplateView.as_view(template_name="index.html")),
                   path('', TemplateView.as_view(template_name="Home.html")),
               ] + static(settings.STATIC_URL,
                          document_root=settings.STATIC_ROOT)
