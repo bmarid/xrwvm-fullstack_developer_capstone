6a3607defe2aee671c1800ca9587faa221e6814c
bmarid <mary.baj@yandex.ru>
2024-05-15 00:01:16 +0200

    prev

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = \
 DEBUG = True
 
 ALLOWED_HOSTS = ['*', 'localhost', 'http://127.0.0.1:3000',
-                    'http://127.0.0.1:8000',
-                    'https://mariiabai-8000.theiadockernext-0-labs-prod-t'
-                    'heiak8s-4-tor01.proxy.cognitiveclass.ai']
+    'http://127.0.0.1:8000',
+    'https://mariiabai-8000.theiadockernext-0-labs-prod-t'
+    'heiak8s-4-tor01.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = [
     'https://mariiabai-8000.theiadockernext-0-labs-prod'
     'theiak8s-4-tor01.proxy.cognitiveclass.ai']
